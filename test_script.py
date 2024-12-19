865b9b4d5bec6dc1ef8fd5efce3769282f976857
chngzhiquan <zhiquanchng@gmail.com>
2024-12-19 20:34:21 +0800

    Scraping Example

--- test_script.py
+++ test_script.py
@@ -1,3 +1,29 @@
-# I want to test pushing this onto the repository
+# Practice scrapping
 
-# More testings
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+# Scraping table
+url = 'https://en.wikipedia.org/wiki/List_of_largest_companies_in_the_United_States_by_revenue'
+page = requests.get(url)
+soup = BeautifulSoup(page.text, features="html.parser")
+
+# Finding titles of table
+table = soup.find_all('table')[0]
+world_titles = table.find_all('th')
+world_table_titles = [title.text.strip() for title in world_titles]
+
+# Adding titles to Dataframe
+df = pd.DataFrame(columns = world_table_titles)
+
+# Finding data in table (tr is for rows and td is for data)
+# Also appends data into Dataframe
+column_data = table.find_all('tr')
+for row in column_data[1:]:
+    row_data = row.find_all('td')
+    individual_row_data = [data.text.strip() for data in row_data]
+    length = len(df)
+    df.loc[length] = individual_row_data
+
+print(df)
