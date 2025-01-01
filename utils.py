30084cd8019f0d42507c9739683723bf627da433
chngzhiquan <zhiquanchng@gmail.com>
2025-01-01 11:25:27 +0800

    changed index to false

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import sqlite3
 
 def copy_from(df, table_name):
     conn = sqlite3.connect('soraka.db')
-    df.to_sql(name=table_name, con=conn, if_exists='replace')
+    df.to_sql(name=table_name, con=conn, if_exists='replace', index=False)
     conn.commit()
 
 
